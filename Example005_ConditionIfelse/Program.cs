e2f01d088e3234625585a75f442e685ec16e03c0
Kristina <40335@mail.ru>
2022-08-08 11:54:50 +0300

    add Example 007 Magic

--- Example005_ConditionIfelse/Program.cs
+++ Example005_ConditionIfelse/Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.WriteLine("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if username == "Маша";
-{   
-    Console.WriteLine("Ура, это же МАША!");
-}
+if(username.ToLower() == "маша")
+{
+Console.WriteLine("Ура, это же МАША!");
+}   
 else
 {
     Console.Write("Привет, ");
